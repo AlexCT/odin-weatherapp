a64e231b00005f9ec23da23a63e6c6c4f4a4485c
AlexCT <tresseltac@gmail.com>
2022-06-29 21:15:47 -0700

    Basic script and styling

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 https://www.theodinproject.com/lessons/node-path-javascript-weather-app
 
 Started June 26 2022
+
+Website: https://alexct.github.io/odin-weatherapp/
+
+Weather data from Openweathermap API.
--- script.js
+++ script.js
@@ -1,6 +1,43 @@
 // Alex Tresselt 6-26-22
 
-// Get location from button
-// return weather data from API
-// process data, only what apps needs
-// display weather
+
+async function getWeather() {
+    // Get city location from text field
+    let loc = document.querySelector('#locationbtn').value;
+
+    // Use Openweathermap's Geocoding API to translate city to lat/long coordinates
+    const reply = await
+        fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + loc + '&limit=1&appid=ecf75d04121014476b6bddba9c305734',
+        {mode: 'cors'});
+    const cityData = await reply.json();
+    let lat = cityData[0].lat;
+    let lon = cityData[0].lon;
+
+    // Get the weather data from Openweathermap's API
+    const response = await 
+        fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat + 
+            '&lon=' + lon + '&appid=ecf75d04121014476b6bddba9c305734&units=imperial', 
+        {mode: 'cors'});
+    const weatherData = await response.json();
+
+    // Display City name
+    const city = document.querySelector('#city');
+    city.innerHTML = weatherData.name;
+    // Display weather icon, using Openweathermap's icons
+    const img = document.querySelector('#weather');
+    img.src = "https://openweathermap.org/img/wn/" + 
+                weatherData.weather[0].icon + "@4x.png";
+    // Display temperature, rounded to nearest degree
+    const temp = document.querySelector('#temp');
+    temp.innerHTML = Math.round(weatherData.main.temp) + "\u00B0F";
+    // Display conditions
+    const conditions = document.querySelector('#conditions');
+    conditions.innerHTML = weatherData.weather[0].description;
+
+}
+
+
+document.getElementById("submitbtn").onclick = function () { 
+    getWeather();
+ };
+
